aab5d6b3056192ed8be7fd4bb83099398683b3a2
Warrioricon <scottcyrusj@gmail.com>
2023-03-07 13:36:05 -0500

    test service wip

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"github.com/ardanlabs/conf"
 	"github.com/dimfeld/httptreemux/v5"
 )
-func main() {
 
+var build = "develop"
+
+func main() {
+	log.Println("starting service", build)
+	defer log.Println("service ended")
+	// Make a channel to listen for an interrupt or terminate signal from the OS.
+	// Use buffered channel because the signal package requires it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("Stopping service")
 }
+
